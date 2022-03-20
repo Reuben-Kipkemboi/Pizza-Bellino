dbf0da8999bd42b93aba0ceeb6bb8132281db620
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-03-20 09:10:55 +0300

    st:order-button - button functioanl, on click order submitted

--- JS/script.js
+++ JS/script.js
@@ -1,82 +1,27 @@
+
+function validate() {
+    if (fullName.value == "" || fullName.value.length == 0) {
+        alert("Dear Customer Enter A valid Name")
+        return false
+    } else if (telephoneNumber.value.length < 10) {
+        alert("Kindly Recheck Your Phone Number")
+        return false
+    } else {
+        alert(fullName.value + " " + "We have received your Pizza order.We are Preparing.Thank You")
+    }
+
+}
 // Showing and hiding form on click of the order link
 $(document).ready(function () {
-
-    let fullName = document.getElementById('name');
-    let telephoneNumber = document.getElementById('tel');
-    let pizzaSize = document.getElementById('pizza-size');
-    let crustType = document.getElementById('crust');
-    let toppingsType = document.getElementById('toppings');
-    let totalNumberOfPizza = document.getElementById('number');
-    let destination = document.getElementById('destination');
-    let btn = document.getElementById('submit')
-
-
-    let priceOfSmall = 450;
-        let priceOfMedium = 650;
-        let priceOfLarge = 950;
-        let crustPrice = 100;
-        let toppingsPrice = 50;
-        let deliveryPrice = 300;
-
-        // form validation
-
-        function validate(){
-            if(fullName.value == "" || fullName.value.length == 0){
-                alert("Dear Customer Enter A valid Name")
-                return false
-            }else if(telephoneNumber.value.length <10){
-                alert("Kindly Recheck Your Phone Number")
-                return false
-            }else {
-                alert(fullName.value +" " + "We have received your Pizza order.We are Preparing.Thank You")
-            }
-
-        }
-
-    function calculatePriceWithDelivery() {
-        if (pizzaSize.value == small) {
-            let small = parseInt(totalNumberOfPizza.value)*(priceOfSmall + crustPrice + toppingsPrice + deliveryPrice);
-            return small
-            
-        }else if (pizzaSize == medium){ 
-            let medium = parseInt(totalNumberOfPizza.value)*(priceOfMedium + crustPrice + toppingsPrice + deliveryPrice);
-            
-        }else {
-            let large = parseInt(totalNumberOfPizza.value)*(priceOfLarge + crustPrice + toppingsPrice + deliveryPrice);
-        }
-
-    }
-    function calculatePriceWithoutDelivery() {
-        if (pizzaSize.value == small) {
-            let small = parseInt(totalNumberOfPizza.value)*(priceOfSmall + crustPrice + toppingsPrice);
-            return small
-            
-        }else if (pizzaSize == medium){ 
-            let medium = parseInt(totalNumberOfPizza.value)*(priceOfMedium + crustPrice + toppingsPrice);
-            
-        }else {
-            let large = parseInt(totalNumberOfPizza.value)*(priceOfLarge + crustPrice + toppingsPrice);
-        }
-
-    }
-
-
-
-
-
 
     $('#orderNow').click(function () {
         $('form#orderForm').toggle();
         $('form#orderForm').css("width", "50%");
-
-
     })
     $('#orderNow').click(function () {
         $('#description').toggle();
         $('#pizza-types').toggle();
         $('#pizza-types2').toggle();
-
-
     });
 
     $('#close').click(function () {
@@ -84,10 +29,8 @@ $(document).ready(function () {
         $('#description').show();
         $('#pizza-types').show();
         $('#pizza-types2').show();
-
-
-
     })
+
 
     //  price on move of the mouse
 
@@ -99,29 +42,21 @@ $(document).ready(function () {
     })
 
 
+    //Backend logic
 
-    // submitting and querying user data
+    $('#submit').click(function (event) {
+        event.preventDefault()
+        let fullName = $('#name').val();
+        let telephoneNumber = $('#tel').val();
+        let pizzaSize = $('#pizza-size option:selected').val();
+        let crustType = $('#crust option:selected').val();
+        let toppings = $('#toppings option:selected').val();
+        let totalNumberOfPizza = $('#pizzaNumber').val();
+        let costOfDelivery = $('#delivery option:selected').val()
+        let destination = $('#destination').val();
 
-    //  $('form#submit').submit(function(event){
-    //     event.preventDefault();
-
-    //     calculatePrice();
-
-
-
-    //  })
-
-
-    btn.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        validate();
-
-        calculatePriceWithDelivery()
-        calculatePriceWithoutDelivery()
-
-        
+        let totalsOfPizza = parseInt(totalNumberOfPizza) * (parseInt(pizzaSize) + parseInt(toppings)
+         + parseInt(crustType) + parseInt(costOfDelivery));
+        alert("Heyyy" + fullName + telephoneNumber + totalsOfPizza)
     });
-
-
 })
